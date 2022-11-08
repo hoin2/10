65236046ba1d577f5daf9b7c7f87ede27668f668
hoin2 <jingyusin20@sookmyung.ac.kr>
2022-11-08 16:32:19 +0900

    01

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void) 
+{
+	char code1='A';
+	int code2=65;
+	
+	printf("code1=%c,(%d)\n",code1,code2);
+	printf("code2=%c,(%d)\n",code2,code2);
+	
+	return 0;
+}
