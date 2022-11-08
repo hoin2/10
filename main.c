885b74288b30ead8d465ffb852f865a6eb84c699
hoin2 <jingyusin20@sookmyung.ac.kr>
2022-11-08 16:36:01 +0900

    04

--- main.c
+++ main.c
@@ -5,12 +5,9 @@
 
 int main(void) 
 {
-	char src[]="The worst things to eat before you sleep";
-	char dst[100];
+	char str[30]="happy C programming";
 	
-	strcpy(dst,src);
-	
-	printf("copied string:%s",dst);
+	printf("문자열\"%s\"의 길이:%i",str,strlen(str));
 	
 	return 0;
 }
