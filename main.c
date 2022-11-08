cd67e617b705d61fa593a0605fea003153f97c50
hoin2 <jingyusin20@sookmyung.ac.kr>
2022-11-08 17:09:53 +0900

    06

--- main.c
+++ main.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(void) 
 {
-	int i;
-	char src[100];
-	FILE*fp;
+	char c;
+	FILE*fp=NULL;
 	
-	fp=fopen("sample.txt","w");
+	fp=fopen("sample.txt","r");
 	
-	for(i=0;i<3;i++){
-		printf("input a word:");
-		scanf("%s",&src);
-			
-		fprintf(fp,"%s\n",src);
-	}
+	if(fp==NULL)
+		printf("파일을 못열음\n");
 	
+	c=fgetc(fp);
+	
+	while((c=fgetc(fp))!=EOF)
+		putchar(c);
+		
 	fclose(fp);
 	
 	return 0;
